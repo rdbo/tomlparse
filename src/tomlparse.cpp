1bbacf5f294221070072f268ac62e36cfb158782
rdbo <rdbodev@gmail.com>
2024-06-27 18:26:03 -0300

    started implementing tokenizer

--- src/tomlparse.cpp
+++ src/tomlparse.cpp
@@ -5,14 +5,53 @@
 
 using namespace tomlparse;
 
+enum class TokenType {
+	Identifier,
+	Equals,
+	Number,
+	OpenBrace,
+	CloseBrace,
+};
+
+struct Token {
+	TokenType type;
+	std::string value;
+};
+
+std::vector<Token>
+tokenize_toml_content(std::string &content)
+{
+	std::vector<Token> tokens = {};
+
+	for (size_t i = 0; i < content.length(); ++i) {
+		auto c = content[i];
+		if (std::isspace(c))
+			continue;
+
+		if (std::isdigit(c)) {
+			if (!tokens.empty() && tokens.back().type == TokenType::Number) {
+				tokens.back().value.push_back(c);
+			} else {
+				tokens.push_back(Token { type: TokenType::Number, value: std::string(1, c) });
+			}
+
+			continue;
+		}
+	}
+
+	return tokens;
+}
+
 std::expected<Toml, Error>
 parse_toml_content(std::string &content)
 {
-	for (auto c: content) {
-		if (std::isspace(c))
-			continue;
-	}
+	auto tokens = tokenize_toml_content(content);
 
+	std::cout << "Token count: " << tokens.size() << std::endl;
+	for (auto &token : tokens) {
+		std::cout << "Token: " << static_cast<int>(token.type) << ", " << token.value << std::endl;
+	}
+	
 	return std::unexpected(Error::MalformedContent);
 }
 
@@ -23,7 +62,5 @@ tomlparse::parse(std::ifstream &file)
 	ss << file.rdbuf();
 
 	auto content = ss.str();
-	std::cout << content << std::endl;
-
 	return parse_toml_content(content);
 }
